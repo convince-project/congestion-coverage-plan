2e9d47de987fc702e059ca3180f47d982627a698
Stefano Bernagozzi <stefano.bernagozzi@iit.it>
2025-02-13 11:02:15 +0100

    added file for calculate tsp
    Signed-off-by: Stefano Bernagozzi <stefano.bernagozzi@iit.it>

---
+++ tsp.py
@@ -0,0 +1,35 @@
+from OccupancyMap import OccupancyMap
+import utils
+import matplotlib.pyplot as plt
+from python_tsp.exact import solve_tsp_dynamic_programming
+
+
+
+
+
+
+def create_matrix_from_occupancy_map(occupancy_map, time):
+    matrix = []
+    occupancies = occupancy_map.get_current_occupancies(time)
+    for i in occupancy_map.get_vertices_list():
+        row = []
+        for j in occupancy_map.get_vertices_list():
+            if i == j:
+                row.append(0)
+            else:
+                edge = occupancy_map.find_edge_from_vertices(i, j)
+                if edge is None:
+                    row.append(99999999)
+                else:
+                    if edge in occupancies.keys():
+                        traverse_time =occupancy_map.get_edge_traverse_time(edge)
+                        if occupancies[edge] >= occupancy_map.find_edge_limit(edge):
+                            row.append(traverse_time["high"])
+                        else:
+                            row.append(traverse_time["low"])
+                    row.append()
+        matrix.append(row)
+    return matrix
+
+
+
